d6bf17d5ff5bdb15ad28e731e05015a16e98e14b
Felix Tröbinger <felix.troebinger@gmail.com>
2019-06-20 18:34:40 +0200

    image view attempt 1

--- app/src/main/java/at/fhooe/mc/android/fhroomfinder/LocatorActivity.java
+++ app/src/main/java/at/fhooe/mc/android/fhroomfinder/LocatorActivity.java
@@ -3,6 +3,7 @@ package at.fhooe.mc.android.fhroomfinder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LocatorActivity extends AppCompatActivity {
@@ -14,8 +15,9 @@ public class LocatorActivity extends AppCompatActivity {
 
         Toolbar tb = findViewById(R.id.locator_toolbar);
         tb.setTitle(R.string.locator);
+        tb.setTitleTextColor(getColor(R.color.white));
         setSupportActionBar(tb);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // TODO make the damn button work
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Room r = getIntent().getParcelableExtra(MainActivity.ROOM_INTENT);
 
@@ -33,5 +35,32 @@ public class LocatorActivity extends AppCompatActivity {
 
         tv = findViewById(R.id.activity_locator_room_name);
         tv.setText(r.getName());
+
+        ImageView iv = findViewById(R.id.activity_locator_floor_plan_image_view);
+        switch (r.getFloor()) {
+            case 0:
+                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_0);
+                break;
+            case 1:
+                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_1);
+                break;
+            case 2:
+                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_2);
+                break;
+            case 3:
+                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_3);
+                break;
+            case 4:
+                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_4);
+                break;
+        }
+
+        ImageView circleView = findViewById(R.id.activity_locator_circle_image_view);
+        circleView.setImageResource(R.drawable.ic_square);
+        circleView.setScaleType(ImageView.ScaleType.FIT_START);
+        circleView.setScaleX(0.2f);
+        circleView.setScaleY(0.2f);
+        //circleView.setX(300);
+
     }
 }
