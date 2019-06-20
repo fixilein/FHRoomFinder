6065711b6a4fc4d45f9bccf06791850b0915b213
Felix Tröbinger <felix.troebinger@gmail.com>
2019-06-20 19:14:01 +0200

    add circle

--- app/src/main/java/at/fhooe/mc/android/fhroomfinder/LocatorActivity.java
+++ app/src/main/java/at/fhooe/mc/android/fhroomfinder/LocatorActivity.java
@@ -1,10 +1,18 @@
 package at.fhooe.mc.android.fhroomfinder;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LocatorActivity extends AppCompatActivity {
 
@@ -21,6 +29,62 @@ public class LocatorActivity extends AppCompatActivity {
 
         Room r = getIntent().getParcelableExtra(MainActivity.ROOM_INTENT);
 
+        fillTextViews(r);
+
+        ImageView iv = findViewById(R.id.activity_locator_floor_plan_image_view);
+
+        Bitmap floorPlan = getFloorPlanBitmap(r.getFloor());
+
+        Canvas canvas = new Canvas(floorPlan);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+
+        int x = 1000;
+        int y = 1000;
+        canvas.drawCircle(x, y, 150, paint);
+
+        iv.setImageBitmap(floorPlan);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LocatorActivity.this, "IV clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private Bitmap getFloorPlanBitmap(int _f) {
+        Bitmap b;
+        switch (_f) {
+            case 0:
+                b = BitmapFactory.decodeResource(getResources(), R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_0).copy(Bitmap.Config.ARGB_8888, true);
+
+                break;
+            case 1:
+                b = BitmapFactory.decodeResource(getResources(), R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_1).copy(Bitmap.Config.ARGB_8888, true);
+
+                break;
+            case 2:
+                b = BitmapFactory.decodeResource(getResources(), R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_2).copy(Bitmap.Config.ARGB_8888, true);
+
+                break;
+            case 3:
+                b = BitmapFactory.decodeResource(getResources(), R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_3).copy(Bitmap.Config.ARGB_8888, true);
+                break;
+            case 4:
+                b = BitmapFactory.decodeResource(getResources(), R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_4).copy(Bitmap.Config.ARGB_8888, true);
+                break;
+            default:
+                Log.e(MainActivity.TAG, "Unrecognized Floor");
+                b = null;
+                break;
+        }
+        return b;
+    }
+
+    private void fillTextViews(Room r) {
         TextView tv;
         tv = findViewById(R.id.activity_locator_room_number);
         tv.setText(r.getToken());
@@ -35,32 +99,6 @@ public class LocatorActivity extends AppCompatActivity {
 
         tv = findViewById(R.id.activity_locator_room_name);
         tv.setText(r.getName());
-
-        ImageView iv = findViewById(R.id.activity_locator_floor_plan_image_view);
-        switch (r.getFloor()) {
-            case 0:
-                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_0);
-                break;
-            case 1:
-                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_1);
-                break;
-            case 2:
-                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_2);
-                break;
-            case 3:
-                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_3);
-                break;
-            case 4:
-                iv.setImageResource(R.mipmap.fhooe_hagenberg_campus_raumplan_fh2_ebene_4);
-                break;
-        }
-
-        ImageView circleView = findViewById(R.id.activity_locator_circle_image_view);
-        circleView.setImageResource(R.drawable.ic_square);
-        circleView.setScaleType(ImageView.ScaleType.FIT_START);
-        circleView.setScaleX(0.2f);
-        circleView.setScaleY(0.2f);
-        //circleView.setX(300);
-
     }
+
 }
